ff3d7d95030fc83f4eabcecfd40109494d4ad862
Shane Johnson <shane.john316@gmail.com>
2024-09-19 12:37:21 -0500

    Added logic for losing when time runs out

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,49 @@
-timerDiv = document.querySelector("#timer");
-startButton = document.querySelector("#start");
+const timerDiv = document.querySelector('#timer');
+const startButton = document.querySelector('#start');
+const winsTitle = document.querySelector('#winsTitle');
+const lossesTitle = document.querySelector('#lossesTitle');
+const winCount = document.querySelector('#winCount');
+const lossCount = document.querySelector('#lossCount');
+const message = document.querySelector('#message');
 
-function countdown() {
-   let timer = 5
+let losses = 0;
+let wins = 0;
+
+function hideTotals() {
+    winsTitle.style.display = 'none';
+    lossesTitle.style.display = 'none';
+};
+
+function displayTotals() {
+    winsTitle.style.display = 'block';
+    lossesTitle.style.display = 'block';
+};
+
+function gameStart() {
+    hideTotals();
+    startButton.style.display = 'none';
+    message.textContent = "";
+
+    let timer = 5
 
     let timeInterval = setInterval(function () {
         timerDiv.textContent = timer;
-        timer --;
+        timer--;
 
         if (timer < 0) {
             clearInterval(timeInterval);
+            displayTotals();
+            losses ++;
+            lossCount.textContent = losses;
+            startButton.style.display = 'block';
+            message.textContent = "GAME OVER!"
         }
     }, 1000)
 }
 
 startButton.addEventListener('click', function (event) {
     event.preventDefault();
-    countdown();
+    gameStart();
 });
+
+hideTotals();
